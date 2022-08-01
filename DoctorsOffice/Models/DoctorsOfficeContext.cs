47357ae8de57a14bc0bb3a7ec715d98507651897
danielware <waredanielb@gmail.com>
2022-08-01 13:03:18 -0700

    Initial file setup.

---
+++ DoctorsOffice/Models/DoctorsOfficeContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace DoctorsOffice.Models
+{
+  public class DoctorsOfficeContext : DbContext
+  {
+    public DbSet<Doctor> Doctors { get; set; }
+    public DbSet<Patient> Patients { get; set; }
+    public DbSet<DoctorPatient> DoctorPatient { get; set; }
+
+    public DoctorsOfficeContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
+  }
+}
